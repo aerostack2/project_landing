2bfb319a6246662752289a2e7fac227e894f5851
keikei014 <fjac014@gmail.com>
2024-12-10 16:10:21 +0100

    Config for basic ship simulation.

---
+++ plugin/build/plugin/CMakeFiles/Surface.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Surface.dir/maritime/Surface.cc.o"
+  "CMakeFiles/Surface.dir/maritime/Surface.cc.o.d"
+  "libSurface.pdb"
+  "libSurface.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Surface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
